9148a88e87f4e1ca77f3f9cc075b393cc940c4bf
Peyton Bechard <pbechard@my.yorku.ca>
2022-08-01 23:26:25 +0800

    Adds HTML elements and styling to form

---
+++ password-form.js
@@ -0,0 +1,7 @@
+/*
+Given a potential password, classify the password into one of four 
+categories: unacceptable, weak, strong, or very strong. The 
+unacceptable category is for passwords that are considered too 
+common. You should create a list of these unacceptable passwords 
+with at least 5 unacceptable passwords in it.  
+*/
